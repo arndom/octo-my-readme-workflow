851ceb5560b14cf0260cc927c27b0b8d5b616df5
arndom <aminonimisi2@gmail.com>
2021-11-15 00:08:20 +0100

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try{
+    
+    const name = core.getInput(get-git-user);
+    console.log(`Hello ${name}`);
+    
+    core.setOutput("result", name);
+    
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+
+}catch(error){
+    core.setFailed(error.message);
+}

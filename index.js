05b10a918d8c889c7e34f38b355de270db47f74b
arndom <aminonimisi2@gmail.com>
2021-11-15 02:13:53 +0100

    test

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-try{
-    const name = core.getInput(git_user);
-    console.log(`Hello ${name}`);
-    
-    // core.setOutput("result", name);
-    
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
-
-}catch(error){
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
     core.setFailed(error.message);
-}
+  }

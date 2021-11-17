7a1e5b6a2a9bd0cd30e461edc5eca4d2c6a2e204
arndom <aminonimisi2@gmail.com>
2021-11-17 10:46:06 +0100

    edit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
     console.log(`Hello ${userName}! This was run at ${time}`);
 
     const mostUsedLanguage = core.getInput('most-used-language');
-    console.log(`your most used language is ${mostUsedLanguage}!`)
+    console.log(`your most used language is ${mostUsedLanguage}!`);
 
   } catch (error) {
     core.setFailed(error.message);

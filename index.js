5087282695427dcce9c8b0a527566679537af669
arndom <aminonimisi2@gmail.com>
2021-11-17 10:06:19 +0100

    edit

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const github = require('@actions/github')
 try {
     // `who-to-greet` input defined in action metadata file
     
-    const nameToGreet = core.getInput('who-to-greet');
+    const userName = core.getInput('who-to-greet');
     const time = (new Date()).toTimeString();
     
-    console.log(`Hello ${nameToGreet}!`);
+    console.log(`Hello ${userName}!`);
 
     console.log(`This was run at ${time}`)
 

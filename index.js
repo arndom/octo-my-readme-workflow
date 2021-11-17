9f7cb4dfa6b26a9c4345dde9c08517b56f74897d
arndom <aminonimisi2@gmail.com>
2021-11-17 10:37:34 +0100

    edit

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ try {
     
     const userName = core.getInput('who-to-greet');
     const time = (new Date()).toTimeString();
-    
-    console.log(`Hello ${userName}!`);
+    console.log(`Hello ${userName}! This was run at ${time}`);
 
-    console.log(`This was run at ${time}`)
+    const mostUsedLanguage = core.getInput('most-used-language');
+    console.log(`your most used language is ${mostUsedLanguage}!`)
 
   } catch (error) {
     core.setFailed(error.message);

5727d8fdf319610a8cb60e932334e6eac529c310
arndom <aminonimisi2@gmail.com>
2021-11-17 10:22:00 +0100

    edit

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ try {
 
     console.log(`This was run at ${time}`)
 
-    // core.setOutput("time", time);
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
-
   } catch (error) {
     core.setFailed(error.message);
   }

573ada7c417ad5f09509f0cd7f4bb51fb982a66d
arndom <aminonimisi2@gmail.com>
2021-11-18 14:31:00 +0100

    edit

--- index.js
+++ index.js
@@ -2,38 +2,66 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const fs = require('fs');
 const child_process =  require('child_process')
+const { Octokit } = require('@octokit/core')
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-try {
+
+(async () => {
+  try {
     // `who-to-greet` input defined in action metadata file
     
     const userName = core.getInput('user');
     const language = core.getInput('language_frequent')
 
-
     console.log(`Hello ${userName}! your most used language in your repos is ${language}`);
 
     const markdown = `${userName}'s' most used language is ${language}`
 
-    child_process.exec('ls', (err, stdout, stderr) => {  
-      if (err) {  
-        console.error(err);  
-        return;  
-      }  
-      console.log(stdout);  
-    });  
+    const username = process.env.GITHUB_REPOSITORY.split("/")[0]
+    const repo = process.env.GITHUB_REPOSITORY.split("/")[1]
+    const getReadme = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: username,
+      repo: repo,
+      path: core.getInput('path'),
+    }).catch(e => {
+      console.error("Failed: ", e)
+      core.setFailed("Failed: ", e.message)
+    })
+    const sha = getReadme.data.sha
 
-    // Readme path, default: ./README.md
-    const README_FILE_PATH = core.getInput('readme_path')
+    console.log(getReadme.data)
 
-    const readmeData = fs.readFileSync(README_FILE_PATH, 'utf8');
-    const newReadme = buildReadme(readmeData, markdown);
+    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      owner: username,
+      repo: repo,
+      path: core.getInput('path'),
+      message: '(Automated) Update README.md',
+      content: Buffer.from(data, "utf8").toString('base64'),
+      sha: sha,
+    }).then(() => {
+      core.setOutput("repositories", Array.from(recentRepos))
+    }).catch((e) => {
+      console.error("Failed: ", e)
+      core.setFailed("Failed: ", e.message)
+    })
 
-    // if there's change in readme file update it
-    if (newReadme !== readmeData) {
-      core.info('Writing to ' + README_FILE_PATH);
-      fs.writeFileSync(README_FILE_PATH, newReadme);
-    }
+
+
+
+    // // Readme path, default: ./README.md
+    // const README_FILE_PATH = core.getInput('readme_path')
+
+    // const readmeData = fs.readFileSync(README_FILE_PATH, 'utf8');
+    // const newReadme = buildReadme(readmeData, markdown);
+
+    // // if there's change in readme file update it
+    // if (newReadme !== readmeData) {
+    //   core.info('Writing to ' + README_FILE_PATH);
+    //   fs.writeFileSync(README_FILE_PATH, newReadme);
+    // }
 
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+)

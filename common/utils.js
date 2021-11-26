6b72abb5d8c9bedf030bc39acb7175cd8276c438
arndom <aminonimisi2@gmail.com>
2021-11-27 00:46:57 +0100

    base test 00

--- common/utils.js
+++ common/utils.js
@@ -1,6 +1,8 @@
 // const {spawn} = require('child_process');
 const core = require('@actions/core');
 const axios = require('axios');
+const fs = require('fs');
+
 
 /**
  * Builds the new readme by replacing the readme's <!-- BLOG-POST-LIST:START --><!-- BLOG-POST-LIST:END --> tags
@@ -82,9 +84,27 @@ const axios = require('axios');
     "typescript": "typescript"
   }
   
+  // function to encode file data to base64 encoded string
+  const base64_encode = (file) => {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+  }
+
+  // function to create file from base64 encoded string
+  const base64_decode = (base64str, file) => {
+    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
+    var bitmap = new Buffer(base64str, 'base64');
+    // write buffer to file
+    fs.writeFileSync(file, bitmap);
+    console.log('******** File created from base64 encoded string ********');
+  }
 
   module.exports = {
       buildReadme,
       request,
-      languagesSupported
+      languagesSupported,
+      base64_decode,
+      base64_encode
   }
--- index.js
+++ index.js
@@ -1,84 +1,78 @@
 const core = require('@actions/core')
 const { Octokit } = require('@octokit/core');
-const { languagesSupported } = require('./common/utils');
+const { languagesSupported, base64_encode } = require('./common/utils');
 const fetchTopLanguages = require('./top-languages-fetcher')
-const { readFile, writeFile } = require('fs')
-const { promisify } = require('util')
-const asyncReadFile = promisify(readFile)
+const fs = require('fs');
 
 const octokit = new Octokit({ auth: core.getInput('gh_token') });
 
 (async () => {
 
   try {    
-    // const username = process.env.GITHUB_REPOSITORY.split("/")[0]
-    // const repo = process.env.GITHUB_REPOSITORY.split("/")[1]
-    // const markdown = `${username}'s' most used language is text`
+    const username = process.env.GITHUB_REPOSITORY.split("/")[0]
+    const repo = process.env.GITHUB_REPOSITORY.split("/")[1]
 
-    // console.log("Hello", username,  ", the workflow is being deployed in the", repo, "repo")
+    var markdown = ``
 
-    // const getLanguage = await fetchTopLanguages();
-    // console.log("Your top language is", Object.keys(getLanguage)[0])
+    console.log("Hello", username,  ", the workflow is being deployed in the", repo, "repo")
 
-    const lang = "java"
+    const getLanguage = await fetchTopLanguages();
+    console.log("Your top language is", Object.keys(getLanguage)[0])
 
-    const returnSvg = async (path) => {
-      const data = await asyncReadFile(path)    
-      return data.toString() 
-    }
+    const lang = Object.keys(getLanguage)[0]
+    // const lang = "javascript"
 
-    if(Object.keys(languagesSupported).map(i => i.toLowerCase()).includes(lang)){
+    if(Object.keys(languagesSupported).map( _lang => _lang.toLowerCase() ).includes(lang)){
+      
       console.log("language supported:", lang)
       
-      var languageIconPath = `./lib/octo-lang/${lang}.jpeg`
+      // path to collection octo-langs
+      var languageIconPath = `./lib/octo-lang/${lang}.png`
       
-      var languageIcon = await returnSvg(languageIconPath)
-        .then((res)=>{
-          return res
-        })
-        .catch((err)=>{
-          console.error(`failed to read svg file: ${err}`)
-        })
+      // convert image to base64 encoded string
+      var base64str = base64_encode(languageIconPath);
 
-      writeFile('./language-icon.jpeg', languageIcon, (err) => {
-        if (err) throw err;               
-        console.log('language logo received');
+      // write file to root dir
+      fs.writeFile("my-octo-lang.png", base64str, 'base64', function(err) {
+        console.log(err);
       });
+
+      markdown =  `![octo-lang](my-octo-lang.png "ocotolang")`
 
     }else{
       console.error("language unsupported:",lang)
     }
 
-    // const getReadme = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-    //   owner: username,
-    //   repo: repo,
-    //   path: core.getInput('readme_path'),
-    // }).then( res => {
-    //   // console.log(res.data)
-    //   return res.data
-    // }     
-    // ).catch(e => {
-    //   console.error("Failed: ", e)
-    //   core.setFailed("Failed: ", e.message)
-    // })
-    // console.log(getReadme)
+    const getReadme = await octokit.request('GET /repos/{owner}/{repo`}/contents/{path}', {
+      owner: username,
+      repo: repo,
+      path: core.getInput('readme_path'),
+    }).then( res => {
+      // console.log(res.data)
+      return res.data
+    }     
+    ).catch(e => {
+      console.error("Failed: ", e)
+      core.setFailed("Failed: ", e.message)
+    })
+    console.log(getReadme)
 
-    // const sha = getReadme.sha
-    // console.log(sha)
+    const sha = getReadme.sha
+    console.log(sha)
 
-    // await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-    //   owner: username,
-    //   repo: repo,
-    //   path: core.getInput('readme_path'),
-    //   message: '(Automated) Update README.md',
-    //   content: Buffer.from(markdown, "utf8").toString('base64'),
-    //   sha: sha,
-    // }).then(() => {
-    //   core.setOutput("result", (markdown))
-    // }).catch((e) => {
-    //   console.error("Failed: ", e)
-    //   core.setFailed("Failed: ", e.message)
-    // })
+    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      owner: username,
+      repo: repo,
+      path: core.getInput('readme_path'),
+      message: '(Automated) Update README.md',
+      content: Buffer.from(markdown, "utf8").toString('base64'),
+      sha: sha,
+    }).then(() => {
+      core.setOutput("result", (markdown))
+    }).catch((e) => {
+      console.error("Failed: ", e)
+      core.setFailed("Failed: ", e.message)
+    })
 
   } catch (error) {
     core.setFailed(error.message);

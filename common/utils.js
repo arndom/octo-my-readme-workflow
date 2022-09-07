7869cb76d4e42c4cd7047f7f2c00a80fa029df50
Nabil Alamin <aminonimisi2@gmail.com>
2022-09-07 18:10:30 +0100

    Update utils.js

--- common/utils.js
+++ common/utils.js
@@ -41,7 +41,7 @@ const fs = require('fs');
   // function to encode file data to base64 encoded string
   const base64_encode = (file) => {
     // read binary data
-    var bitmap = fs.readFileSync(file);
+    let bitmap = fs.readFileSync(file);
     // convert binary data to base64 encoded string
     return new Buffer(bitmap).toString('base64');
   }
@@ -49,7 +49,7 @@ const fs = require('fs');
   // function to create file from base64 encoded string
   const base64_decode = (base64str, file) => {
     // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-    var bitmap = new Buffer(base64str, 'base64');
+    let bitmap = new Buffer(base64str, 'base64');
     // write buffer to file
     fs.writeFileSync(file, bitmap);
     console.log('******** File created from base64 encoded string ********');
